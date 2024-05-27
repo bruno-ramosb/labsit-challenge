a62344cf5a5a619037a6bc7982fdbc44a17f3024
bruno.silva <69005785+bruno-ramosb@users.noreply.github.com>
2024-05-27 17:30:30 -0300

    Corrigindo namespaces

---
+++ src/Labsit.Application/Features/BankAccount/Handlers/Query/GetBankAccountByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Labsit.Application.Common.Constants;
+using Labsit.Application.Common.Response;
+using Labsit.Application.Features.BankAccount.Commands.Query;
+using Labsit.Application.Features.BankAccount.Responses.Query;
+using Labsit.Domain.Contracts.Repositories;
+using MediatR;
+
+namespace Labsit.Application.Features.BankAccount.Handlers.Query
+{
+    public class GetBankAccountByIdQueryHandler(IValidator<GetBankAccountByIdQuery> validator,
+                                     IBankAccountRepository repository)
+                                    : IRequestHandler<GetBankAccountByIdQuery, Result<BankAccountQueryModel>>
+    {
+        public async Task<Result<BankAccountQueryModel>> Handle(GetBankAccountByIdQuery request, CancellationToken cancellationToken)
+        {
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+                return Result<BankAccountQueryModel>.Fail(validationResult.Errors);
+
+            var result = await repository.GetByIdAsync(request.Id);
+            if (result is null)
+                return Result<BankAccountQueryModel>.Fail(Messages.BANK_ACCOUNT_NOT_FOUND);
+
+            var model = new BankAccountQueryModel(result.Id,
+                result.BranchCode,
+                result.AccountNumber,
+                result.Balance,
+                result.TotalCreditLimit,
+                result.AvailableCreditLimit,
+                result.CustomerId);
+
+            return Result<BankAccountQueryModel>.Successful(model);
+        }
+    }
+}
--- src/Labsit.Application/Features/Card/Handlers/Query/GetCardByIdQueryHandler.cs
+++ src/Labsit.Application/Features/Card/Handlers/Query/GetCardByIdQueryHandler.cs
@@ -1,8 +1,6 @@
 ﻿using FluentValidation;
 using Labsit.Application.Common.Constants;
 using Labsit.Application.Common.Response;
-using Labsit.Application.Features.BankAccount.Commands.Query;
-using Labsit.Application.Features.BankAccount.Responses.Query;
 using Labsit.Application.Features.Card.Command.Query;
 using Labsit.Application.Features.Card.Responses.Query;
 using Labsit.Domain.Contracts.Repositories;
@@ -33,33 +31,6 @@ namespace Labsit.Application.Features.Card.Handlers.Query
                 result.BankAccountId);
 
             return Result<CardQueryModel>.Successful(model);
-        }
-    }
-
-
-    public class GetBankAccountByIdQueryHandler(IValidator<GetBankAccountByIdQuery> validator,
-                                     IBankAccountRepository repository)
-                                    : IRequestHandler<GetBankAccountByIdQuery, Result<BankAccountQueryModel>>
-    {
-        public async Task<Result<BankAccountQueryModel>> Handle(GetBankAccountByIdQuery request, CancellationToken cancellationToken)
-        {
-            var validationResult = await validator.ValidateAsync(request, cancellationToken);
-            if (!validationResult.IsValid)
-                return Result<BankAccountQueryModel>.Fail(validationResult.Errors);
-
-            var result = await repository.GetByIdAsync(request.Id);
-            if (result is null)
-                return Result<BankAccountQueryModel>.Fail(Messages.BANK_ACCOUNT_NOT_FOUND);
-
-            var model = new BankAccountQueryModel(result.Id,
-                result.BranchCode,
-                result.AccountNumber,
-                result.Balance,
-                result.TotalCreditLimit,
-                result.AvailableCreditLimit,
-                result.CustomerId);
-
-            return Result<BankAccountQueryModel>.Successful(model);
         }
     }
 }
